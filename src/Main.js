79625aa32fd54c8fe248d670a7f3546a4178aa6e
Surasak Phothiphiphit <hrll0102@hotmail.com>
2018-04-12 12:32:23 +0700

    Fix wrong word

--- src/Main.js
+++ src/Main.js
@@ -23,7 +23,7 @@ export default {
       }
     } else {
       observer = new Observer(connection, opts)
-      Vue.prototype.$socket = observer.WebSockekt
+      Vue.prototype.$socket = observer.WebSocket
     }
 
     Vue.mixin({
